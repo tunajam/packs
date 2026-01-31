f210d357fa0a74bcc52e27445828e9d98eb79a65
Hunter Bacot <hsbacot@gmail.com>
2026-01-30 16:37:27 -0800

    feat: styled --help output with lipgloss colors

--- cmd/packs/main.go
+++ cmd/packs/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/charmbracelet/lipgloss"
 	"github.com/spf13/cobra"
 	"github.com/tunajam/packs/internal/commands"
 )
@@ -24,6 +25,49 @@ var (
 	date    = "unknown"
 )
 
+var (
+	titleStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("170")).Bold(true)
+	cmdStyle     = lipgloss.NewStyle().Foreground(lipgloss.Color("42"))
+	descStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("252"))
+	dimStyle     = lipgloss.NewStyle().Foreground(lipgloss.Color("241"))
+	accentStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("170"))
+)
+
+func styledHelp(cmd *cobra.Command, args []string) {
+	fmt.Println()
+	fmt.Println(accentStyle.Render(banner))
+	
+	fmt.Println(titleStyle.Render("  USAGE"))
+	fmt.Printf("    %s\n\n", dimStyle.Render("packs [command]"))
+	
+	fmt.Println(titleStyle.Render("  COMMANDS"))
+	fmt.Printf("    %s  %s\n", cmdStyle.Render("packs             "), descStyle.Render("Browse packs in TUI"))
+	fmt.Printf("    %s  %s\n", cmdStyle.Render("packs find <query>"), descStyle.Render("Search for packs"))
+	fmt.Printf("    %s  %s\n", cmdStyle.Render("packs get <name>  "), descStyle.Render("Install a pack"))
+	fmt.Printf("    %s  %s\n", cmdStyle.Render("packs info <name> "), descStyle.Render("Show pack details"))
+	fmt.Printf("    %s  %s\n", cmdStyle.Render("packs submit <ref>"), descStyle.Render("Submit a pack to registry"))
+	fmt.Println()
+	
+	fmt.Println(titleStyle.Render("  GITHUB FETCH"))
+	fmt.Printf("    %s\n", cmdStyle.Render("packs get gh:user/repo/path/to/pack"))
+	fmt.Printf("    %s\n\n", dimStyle.Render("Fetch directly from GitHub (works with private repos)"))
+	
+	fmt.Println(titleStyle.Render("  AUTH"))
+	fmt.Printf("    %s  %s\n", cmdStyle.Render("packs login "), descStyle.Render("Authenticate with GitHub"))
+	fmt.Printf("    %s  %s\n", cmdStyle.Render("packs logout"), descStyle.Render("Log out"))
+	fmt.Printf("    %s  %s\n", cmdStyle.Render("packs whoami"), descStyle.Render("Show current user"))
+	fmt.Println()
+	
+	fmt.Println(titleStyle.Render("  OPTIONS"))
+	fmt.Printf("    %s  %s\n", cmdStyle.Render("-j, --json    "), descStyle.Render("Output as JSON"))
+	fmt.Printf("    %s  %s\n", cmdStyle.Render("    --no-cache"), descStyle.Render("Bypass local cache"))
+	fmt.Printf("    %s  %s\n", cmdStyle.Render("-h, --help    "), descStyle.Render("Show this help"))
+	fmt.Printf("    %s  %s\n", cmdStyle.Render("-v, --version "), descStyle.Render("Show version"))
+	fmt.Println()
+	
+	fmt.Printf("  %s %s\n\n", dimStyle.Render("Docs:"), accentStyle.Render("https://packs.sh"))
+}
+
 func main() {
 	rootCmd := &cobra.Command{
 		Use:   "packs",
@@ -34,6 +78,11 @@ func main() {
 			commands.RunTUI()
 		},
 	}
+	
+	// Custom help
+	rootCmd.SetHelpFunc(func(cmd *cobra.Command, args []string) {
+		styledHelp(cmd, args)
+	})
 
 	// Version flag
 	rootCmd.Version = version
